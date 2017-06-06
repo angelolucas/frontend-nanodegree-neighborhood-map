db0d852250ff62c2deb926afdf4e05ab59d2c87c
Ângelo Lucas <angelo.lrc@gmail.com>
2017-06-06 20:59:02 -0300

    knockoutJS

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
-var locations = {
-  stadium: {
+// Locations
+var locations = [
+  {
+    name: 'estadio',
     position: {lat: -15.796, lng: -47.95},
     title: "Estadio de Futebol",
     description: "Estádio Nacional de Brasília Mané Garrincha, também conhecido como simplesmente Mané Garrincha, é um estádio de futebol e arena multiuso brasileiro, situado em Brasília, no Distrito Federal."
   },
-  museum: {
+  {
+    name: 'museu',
     position: {lat: -15.796, lng: -47.90},
     title: "Museu",
     description: "Museus de Brasília O Museu da Fotografia Documental www.mfd.mus.br é um web museu nascido em Brasília com características bem definidas"
   },
-  whatever: {
+  {
+    name: 'whatever',
     position: {lat: -15.796, lng: -47.85},
     title: "Whatever",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever "
   },
+]
+
+function AppViewModel() {
+  var self = this;
+
+  self.locations = ko.observableArray(locations);
 }
 
+ko.applyBindings(new AppViewModel());
+
+// Init Map
 function initMap() {
   var uluru = {lat: -15.79, lng: -47.91};
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -48,6 +61,7 @@ var clickMarker = function(key) {
   });
 }
 
+// Open Informations About the Location
 var openLocation = function(key) {
   var marker = locations[key].marker;
   // Remove animation from all markers
@@ -69,7 +83,7 @@ var openLocation = function(key) {
   appendContent('p', locations[key].description, singleBox);
 }
 
-// Recreates Content
+// Update Content
 var appendContent = function(tag, content, container){
   var tag = document.createElement(tag);
   var content = document.createTextNode(content);
@@ -77,23 +91,7 @@ var appendContent = function(tag, content, container){
   container.appendChild(tag);
 };
 
-// List Sidebar
-var appendSidebar = function() {
-  var listSidebar = document.querySelector('.navigation__list');
-  for(i in locations) {
-    var item = document.createElement('li');
-    item.setAttribute('class', 'navigation__item');
-    item.setAttribute('data-item', i);
-    item.setAttribute('onclick', 'openLocation("' + i + '")');
-    var a = document.createElement('a');
-    var title = document.createTextNode(locations[i].title);
-    item.appendChild(a);
-    a.appendChild(title);
-
-    listSidebar.appendChild(item);
-  }
-}
-
+// Show and Hide Navigation
 var toggleNavigation = function() {
   var body = document.querySelector('body');
 
@@ -104,7 +102,7 @@ var toggleNavigation = function() {
   }
 }
 
-// Toggle navigation
+// Closed Navigation for Mobile
 if (document.body.clientWidth > 600) {
   toggleNavigation();
 }
@@ -112,10 +110,6 @@ var toggleButton = document.querySelector('.navigation__toggle');
 toggleButton.addEventListener('click', function() {
   toggleNavigation();
 })
-
-
-
-appendSidebar();
 
 // Click item in Sidebar
 var sidebarItem = document.querySelector('.navigation__item');
